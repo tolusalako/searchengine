9120e4ec997aa704c1d686231016418ddfbc7351
Douglas Huynh <douglah1@uci.edu>
2017-03-10 00:53:38 -0800

    Implement json-based query instead of using URI, allows boolean matching of elements and boosting at query time

--- src/main/java/cs121/controllers/SearchController.java
+++ src/main/java/cs121/controllers/SearchController.java
@@ -2,9 +2,12 @@ package cs121.controllers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
@@ -19,12 +22,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
 import cs121.dto.QueryItem;
 import io.undertow.util.StatusCodes;
 
@@ -44,13 +45,59 @@ public class SearchController {
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ModelAndView resultsPage(Model model, @RequestBody MultiValueMap<String, String> body) {
-        String query = body.get("query").get(0).replaceAll(" ", "+");
+        String query = body.get("query").get(0);
         List<QueryItem> result = new ArrayList<>();
         try {
-            Response res = client.performRequest("GET", "html_index/_search?q=tokens:" + query);
+        	
+        	JsonObject rootObject = new JsonObject();
+        	JsonObject boolObject = new JsonObject();
+        	JsonObject shouldObject = new JsonObject();
+        	
+        	rootObject.add("query", boolObject);
+        	boolObject.add("bool", shouldObject);
+        	
+        	JsonObject shouldMatchTitle = new JsonObject();
+        	JsonObject title = new JsonObject();
+        	title.addProperty("query", query);
+        	title.addProperty("boost", 6);
+        	shouldMatchTitle.add("title", title);
+        	JsonObject matchTitle = new JsonObject();
+        	matchTitle.add("match", shouldMatchTitle);
+        	
+        	JsonObject shouldMatchHeaders = new JsonObject();
+        	JsonObject headers = new JsonObject();
+        	headers.addProperty("query", query);
+        	headers.addProperty("boost", 4);
+        	shouldMatchHeaders.add("header_tokens", headers);
+        	JsonObject matchHeaders = new JsonObject();
+        	matchHeaders.add("match", shouldMatchHeaders);
+        	
+        	JsonObject shouldMatchBold = new JsonObject();
+        	JsonObject bold = new JsonObject();
+        	bold.addProperty("query", query);
+        	bold.addProperty("boost", 2);
+        	shouldMatchBold.add("bold_tokens", bold);
+        	JsonObject matchBold = new JsonObject();
+        	matchBold.add("match", shouldMatchBold);
+        	
+        	
+        	JsonObject shouldMatchTokens = new JsonObject();
+        	shouldMatchTokens.addProperty("tokens", query);
+        	JsonObject matchTokens = new JsonObject();
+        	matchTokens.add("match", shouldMatchTokens);
+
+        	JsonArray shouldArray = new JsonArray();
+        	shouldArray.add(matchTokens);
+        	shouldArray.add(matchTitle);
+
+        	shouldObject.add("should", shouldArray);
+
+        	HttpEntity entity = new NStringEntity(rootObject.toString(), ContentType.APPLICATION_JSON);
+        	
+        	Response res = client.performRequest("GET", "html_index/_search", Collections.<String, String> emptyMap(), entity);
 
             if (res.getStatusLine().getStatusCode() != StatusCodes.OK) {
-                LOG.error("Could not complete request");
+                LOG.error("Could not complete search request for query: {}", query);
             }
 
             else {
@@ -78,20 +125,20 @@ public class SearchController {
                         url = "http://" + url;
                     }
                     
-                    StringBuilder title = new StringBuilder();
+                    StringBuilder titleString = new StringBuilder();
                     try {
                     	JsonArray titleArray = source.getAsJsonArray("title");
                     	Iterator<JsonElement> iterator = titleArray.iterator();
                     	while (iterator.hasNext()) {
-                    		title.append(iterator.next().getAsString());
-                    		title.append(" ");
+                    		titleString.append(iterator.next().getAsString());
+                    		titleString.append(" ");
                     	}
                     }
                     catch (Exception e) {
-                    	title.append("");
+                    	titleString.append("");
                     }
                     
-                    result.add(new QueryItem(title.toString(), url, builder.toString(), obj.get("_score").getAsDouble()));
+                    result.add(new QueryItem(titleString.toString(), url, builder.toString(), obj.get("_score").getAsDouble()));
                     LOG.info(obj.toString());
                 }
 
